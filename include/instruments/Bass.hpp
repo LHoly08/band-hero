aef77008c1c89a5db334b10b9991f546afe8a2d9
LHoly08 <santosdiasluis08@gmail.com>
2026-06-22 17:49:06 +0100

    Implement getPlay method in Bass class
    Add getPlay method to Bass class for handling play logic.

--- include/instruments/Bass.hpp
+++ include/instruments/Bass.hpp
@@ -14,6 +14,18 @@ public:
   }
 };
 
+inline bool getPlay(std::uint32_t &val) const noexcept {
+  if constexpr (Dif == Difficulty::EASY) {
+    for (std::uint8_t i{}, string = (val & 0x1F);
+      i < m_bass.getNumberStrings();
+      string = (val >> (++i * 5)) & 0x1F) {
+
+      val |= (string != 0) * (0x1F << (i * 5));
+    }
+  }
+  Instrument::getPlay(val);
+}
+
 template <Difficulty Dif>
 Bass<Dif>::Bass(std::string filename) noexcept
     : Instrument<Dif>("bass/" + filename) {}
