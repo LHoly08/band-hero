72f97ae77ad2327d41d749dcf1c9e243cb66976d
LHoly08 <santosdiasluis08@gmail.com>
2026-06-22 18:19:03 +0100

    Implement getPlay method in Guitar class
    Add getPlay method for Guitar class to handle play logic based on difficulty.

--- include/instruments/Guitar.hpp
+++ include/instruments/Guitar.hpp
@@ -12,6 +12,18 @@ public:
   inline constexpr std::uint8_t getNumberStrings() const noexcept {
     return NumberStrings<Dif>::Guitar;
   }
+
+  inline bool getPlay(std::uint32_t &val) const noexcept override {
+    if constexpr (Dif == Difficulty::EASY) {
+      for (std::uint8_t i{}, string = (val & 0x1F);
+        i < NumberStrings<Dif>::Bass;
+        string = (val >> (++i * 5)) & 0x1F) {
+
+        val |= (string != 0) * (0x1F << (i * 5));
+      }
+    }
+    return Instrument::getPlay(val);
+  }
 };
 
 template <Difficulty Dif>
