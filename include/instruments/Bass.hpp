7e9fdd8c02c92cd4435b6a8c1be4326c991c4480
LHoly08 <santosdiasluis08@gmail.com>
2026-06-22 17:51:47 +0100

    Refactor getPlay in Bass class
    Refactor getPlay method and remove redundant code.

--- include/instruments/Bass.hpp
+++ include/instruments/Bass.hpp
@@ -9,22 +9,18 @@ template <Difficulty Dif> class Bass final : public Instrument<Dif> {
 public:
   explicit Bass(std::string filename) noexcept;
 
-  inline constexpr std::uint8_t getNumberStrings() const noexcept {
-    return NumberStrings<Dif>::Bass;
+  inline bool getPlay(std::uint32_t &val) const noexcept {
+    if constexpr (Dif == Difficulty::EASY) {
+      for (std::uint8_t i{}, string = (val & 0x1F);
+        i < NumberStrings<Dif>::Bass;
+        string = (val >> (++i * 5)) & 0x1F) {
+
+        val |= (string != 0) * (0x1F << (i * 5));
+      }
+    }
+    return Instrument::getPlay(val);
   }
 };
-
-inline bool getPlay(std::uint32_t &val) const noexcept {
-  if constexpr (Dif == Difficulty::EASY) {
-    for (std::uint8_t i{}, string = (val & 0x1F);
-      i < m_bass.getNumberStrings();
-      string = (val >> (++i * 5)) & 0x1F) {
-
-      val |= (string != 0) * (0x1F << (i * 5));
-    }
-  }
-  Instrument::getPlay(val);
-}
 
 template <Difficulty Dif>
 Bass<Dif>::Bass(std::string filename) noexcept
