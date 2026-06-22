16b38216be37a2b87c98214cd06a870b16c08ac9
LHoly08 <santosdiasluis08@gmail.com>
2026-06-22 18:20:04 +0100

    Update Guitar.hpp

--- include/instruments/Guitar.hpp
+++ include/instruments/Guitar.hpp
@@ -9,14 +9,10 @@ template <Difficulty Dif> class Guitar final : public Instrument<Dif> {
 public:
   explicit Guitar(std::string filename) noexcept;
 
-  inline constexpr std::uint8_t getNumberStrings() const noexcept {
-    return NumberStrings<Dif>::Guitar;
-  }
-
   inline bool getPlay(std::uint32_t &val) const noexcept override {
     if constexpr (Dif == Difficulty::EASY) {
       for (std::uint8_t i{}, string = (val & 0x1F);
-        i < NumberStrings<Dif>::Bass;
+        i < NumberStrings<Dif>::Guitar;
         string = (val >> (++i * 5)) & 0x1F) {
 
         val |= (string != 0) * (0x1F << (i * 5));
